c18734f6ebe94fb7c283261d81e8a8d775b69da5
Heisey <jheisler01@gmail.com>
2024-07-10 21:52:11 -0700

    linked core to client

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,12 +1,13 @@
 
 import * as React from 'react'
+import * as Core from 'core'
 
 export interface AppProps extends React.PropsWithChildren {
 
 }
 
 const App: React.FC<AppProps> = (props) => {
-
+  const junk = (args: Core.I.Record) => {}
   return (
     <div>
       App
